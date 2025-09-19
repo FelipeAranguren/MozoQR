6d427125ba27923dc1ca5daf7e250570012b1b72
FelipeAranguren <felipearanguren03@gmail.com>
2025-09-18 23:59:47 -0300

    version funcional sin seg

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,20 +1,15 @@
+// backend/config/admin.ts
+
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', '6z0u3EBsS6Vc++psWNiDKuTfzw/NAFCojbrVE0QWmco='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'NGhq0GjFk6WQUVOBvmRUf47H7tnJ0OmMI7WsEQQQaEs='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'CLxLKF+87vtZiUo4bXPPNQ3i5CQMMb7PHkHObtSdrxI='),
     },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
--- backend/dist/config/admin.js
+++ backend/dist/config/admin.js
@@ -1,22 +1,16 @@
 "use strict";
+// backend/config/admin.ts
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     auth: {
-        secret: env('ADMIN_JWT_SECRET'),
+        secret: env('ADMIN_JWT_SECRET', '6z0u3EBsS6Vc++psWNiDKuTfzw/NAFCojbrVE0QWmco='),
     },
     apiToken: {
-        salt: env('API_TOKEN_SALT'),
+        salt: env('API_TOKEN_SALT', 'NGhq0GjFk6WQUVOBvmRUf47H7tnJ0OmMI7WsEQQQaEs='),
     },
     transfer: {
         token: {
-            salt: env('TRANSFER_TOKEN_SALT'),
+            salt: env('TRANSFER_TOKEN_SALT', 'CLxLKF+87vtZiUo4bXPPNQ3i5CQMMb7PHkHObtSdrxI='),
         },
-    },
-    secrets: {
-        encryptionKey: env('ENCRYPTION_KEY'),
-    },
-    flags: {
-        nps: env.bool('FLAG_NPS', true),
-        promoteEE: env.bool('FLAG_PROMOTE_EE', true),
     },
 });
